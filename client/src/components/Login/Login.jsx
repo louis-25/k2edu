93f03c00c2388c3387d68b52fa57fc703f7941e2
louis-25 <20151577@vision.hoseo.edu>
2021-08-31 10:54:36 +0900

    login post요청

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -1,14 +1,39 @@
 import React, { useRef } from 'react';
 import style from './Login.module.css'
 import AuthService from '../../service/auth';
+import axios from 'axios';
 
 function Login(props) {    
+
+  const username = useRef()
+  const password = useRef()
+
+  const postLogin = async(e) =>{
+    e.preventDefault()
+    // console.log('test')
+    // const result = await fetch('http://localhost:8081/auth/login',{
+    //   method: 'POST',
+    //   headers: {'Content-Type':'application/json'},
+    //   body: JSON.stringify({'username':username.current.value, 'password':password.current.value})
+    // })
+    // console.log('result ',result)
+    axios({
+      method: 'POST',
+      url: 'http://localhost:8081/auth/login',
+      data: {'username': username.current.value, 'password': password.current.value},
+      responseType: 'json'
+    }).then((result)=>{
+      console.log(result)
+    })
+    
+  }
+
   return (
     <div>
       <form action="http://localhost:8081/auth/login" method="post" encType="application/json">
         <input type="text" name="username" ref={username} placeholder="id"/><br/>
         <input type="password" name="password" ref={password} placeholder="pw"/><br/>
-        <button type='submit'>전송</button>
+        <button type='submit' onClick={postLogin}>전송</button>
       </form>      
     </div>
   );
