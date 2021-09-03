13cbf0ba4619706e460237970a22bac506d59e42
louis-25 <20151577@vision.hoseo.edu>
2021-09-03 17:59:04 +0900

    swiper설치

--- client/src/components/Main/Visual/Visual.jsx
+++ client/src/components/Main/Visual/Visual.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
-import style from './Visual.module.css'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/swiper.css';
+
 
 function Visual(props) {
   return (
-    <div className={style.main}>
-      <div>main section</div>
-    </div>
+    <Swiper
+      spaceBetween={50}
+      slidesPerView={3}
+      onSlideChange={() => console.log('slide change')}
+      onSwiper={(swiper) => console.log(swiper)}
+    >
+      <SwiperSlide>Slide 1</SwiperSlide>
+      <SwiperSlide>Slide 2</SwiperSlide>
+      <SwiperSlide>Slide 3</SwiperSlide>
+      <SwiperSlide>Slide 4</SwiperSlide>      
+    </Swiper>
   );
 }
 
